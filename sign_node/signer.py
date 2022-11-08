ebb4c8797b159a793788c961b159e936ac4ba9b6
Vasily Kleschov <vkleschov@cloudlinux.com>
2022-11-08 15:11:06 +0100

    Fixed review comments

--- sign_node/signer.py
+++ sign_node/signer.py
@@ -15,6 +15,7 @@ import traceback
 import tempfile
 import typing
 import urllib.parse
+from collections import defaultdict
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from datetime import datetime
 from urllib3 import Retry
@@ -111,20 +112,17 @@ class Signer(object):
             password = self.__password_db.get_password(
                 payload['key_id']
             )
-            sig_type = payload.get('sig_type', 'detach-sign')
-            if not sig_type:
-                sig_type = 'detach-sign'
-            if sig_type == 'clear-sign':
-                cmd_param = '--clear-sign'
-            else:
-                cmd_param = '--detach-sign'
+            sig_type = defaultdict(
+                lambda: '--detach-sign',
+                **{'clear-sign': '--clear-sign',
+                   'detach-sign': '--detach-sign'})
             with tempfile.NamedTemporaryFile(mode='w') as fd:
                 asc_file_name = f'{fd.name}.asc'
                 fd.write(payload['content'])
                 fd.flush()
                 sign_cmd = plumbum.local['gpg'][
-                    '--yes', cmd_param, '--armor',
-                    '--default-key', payload['key_id'], fd.name
+                    '--yes', sig_type[payload.get('sig_type', 'detach-sign')],
+                    '--armor', '--default-key', payload['key_id'], fd.name
                 ]
                 out, status = pexpect.run(
                     command=' '.join(sign_cmd.formulate()),
