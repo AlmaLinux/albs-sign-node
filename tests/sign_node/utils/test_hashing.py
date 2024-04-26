5fc8aa9419e718a3981e593604ecec45fc12b4bf
Igor Sudak <s4inbox@gmail.com>
2024-04-26 13:34:30 +0200

    Added tests for utils/codenotary.py and utils/hashing.py (#86)

---
+++ tests/sign_node/utils/test_hashing.py
@@ -0,0 +1,9 @@
+from sign_node.utils import hashing
+
+
+def test_hashing():
+    sha1_hasher = hashing.get_hasher("sha")
+    assert sha1_hasher.name == 'sha1'
+    sha256_hasher = hashing.get_hasher("sha256")
+    assert sha256_hasher.name == 'sha256'
+
